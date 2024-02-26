d2b7efe6770574185d20052918579bf943d64e7b
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-26 14:37:10 +0300

    register Client model in admin

--- mailer/admin.py
+++ mailer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailer.models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email',)
+    search_fields = ('name', 'email',)
