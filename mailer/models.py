c273f7a9cb1d47823c28714c7b00711d19a7deb6
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-02-26 14:33:38 +0300

    add Client model

--- mailer/models.py
+++ mailer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Client(models.Model):
+
+    name = models.CharField(max_length=150, verbose_name='ФИО')
+    email = models.EmailField(unique=True, verbose_name='почта')
+    comment = models.TextField(**NULLABLE, verbose_name='комментарий')
+
+    def __str__(self):
+        return f'{self.name}, e-mail: {self.email}'
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
