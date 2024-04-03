8562078e35a88c20373a53306c75c3a347aa0b0d
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-03 21:31:06 +0300

    add register Mailing, Message, MailingLog models in admin

--- mailer/admin.py
+++ mailer/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 
-from mailer.models import Client
+from mailer.models import Client, Mailing, Message, MailingLog
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'email',)
     search_fields = ('name', 'email',)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'end_time', 'frequency', 'status')
+    list_filter = ('message',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', )
+    search_fields = ('subject', 'body',)
+
+
+@admin.register(MailingLog)
+class MailingLogAdmin(admin.ModelAdmin):
+    list_display = ('last_try', 'status', 'response', )
+    search_fields = ('status', 'response',)
